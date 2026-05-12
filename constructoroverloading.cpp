9bea01a7aff223a3171608ac0f43c735b0afdbef
Denay2468 <denayaung13@gmail.com>
2026-05-12 11:10:33 +0630

    create function outside of class

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,3 +13,24 @@ class Student {
         Student(int iStudentId, string iName);
         void print();
 };
+
+Student :: Student(){
+}
+
+Student :: Student(int iStudentId){
+    studentId = iStudentId; // definition for ID only
+}
+
+Student :: Student(string iName){
+    name = iName; // definition for NAME only
+}
+
+Student :: Student(int iStudentId, string iName){
+    studentId = iStudentId; // definition for ID and NAME
+    name = iName;
+}
+
+void Student::print(){
+    cout << endl << "Student ID = " << studentId << endl;
+    cout << "Name = " << name << endl;
+}
