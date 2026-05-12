280e133306775c10c3a336c995d572b6cc8d7c68
Denay2468 <denayaung13@gmail.com>
2026-05-12 09:33:42 +0630

    create static funtion and function call inside the main function

--- staticmember.cpp
+++ staticmember.cpp
@@ -16,3 +16,25 @@ public:
         setID();
     }
 };
+
+// assign a value to the studentID variable from the Student class
+int Student::studentID = 10;
+
+// implementing procedures - procedures outside the class
+void Student::setID() {
+    id = ++studentID;
+};
+
+void Student::printAll() {
+    cout << "ID   : " << id << endl;
+    cout << "Name : " << name << endl;
+    cout << endl;
+};
+
+int main() {
+    // creating objects and assigning values
+    Student s1("Lia Kurnia");
+    Student s2("Asroni");
+    Student s3("Andi Kurniawan");
+    Student s4("Joko Punho");  // partially visible
+}
